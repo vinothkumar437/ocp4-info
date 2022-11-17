48726f53e5003f664c9c6609c10a10b5b3d69956
Vinothkumar <32629725+vinothkumar437@users.noreply.github.com>
2022-11-17 08:54:43 +0530

    Create worker.sh

---
+++ custom-scripts/worker.sh
@@ -0,0 +1,10 @@
+for index in $(seq 0 2); do
+    MASTER_HOSTNAME="worker$index.abc.example.com\n"
+    python -c "import base64, json, sys;
+ignition = json.load(sys.stdin);
+files = ignition['storage'].get('files', []);
+files.append({'path': '/etc/hostname', 'mode': 420, 'contents': {'source': 'data:text/plain;charset=utf-8;base64,' + base64.standard_b64encode(b'$MASTER_HOSTNAME').decode().strip(), 'verification': {}}, 'filesystem': 'root'});
+storage['files'] = files;
+ignition['storage'] = storage
+json.dump(ignition, sys.stdout)" <master.ign >"$INFRA_ID-master$index-ignition.json"
+done
